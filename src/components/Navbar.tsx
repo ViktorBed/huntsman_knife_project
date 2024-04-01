a2d785cf560c021912b53a51d7df88aa41966226
ViktorBed <vbednarskii@gmail.com>
2024-04-01 22:41:23 +0300

    build up

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import {logoImg} from "../utils";
+
+export function Navbar() {
+    return (
+        <>
+        <header>
+            <nav>
+                <img src={logoImg}/>
+            </nav>
+        </header>
+        </>
+    );
+}
